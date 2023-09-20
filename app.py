503a22ed725cc07fe56646e4ba6db05df68e58a2
Laithanator <Nukholaith@gmail.com>
2023-09-20 14:00:48 -0400

    day 2

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-import turtle
-t = turtle
-t.shape("turtle")
-t.speed(3)
-def star (space):
-    for i in range(6):
-        t.forward(space)
-        t.left(144)
-    t.right(space)
-    t.forward(space)
-    space = space + 1
-
-star(1)
+name = input("wei-ming is a baddie") 
+x = name.split( )
+print(x[0])
