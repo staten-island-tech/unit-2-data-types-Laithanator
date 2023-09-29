a719d40f786d36ed7ef098c0e7c49f3ace738c9d
Laithanator <144480666+Laithanator@users.noreply.github.com>
2023-09-29 14:04:37 -0400

    day 8

--- GCF.py
+++ GCF.py
@@ -1,11 +1,15 @@
-number = int(input("Please input a number: "))
-
-def operation():
-    factor = 0
-    divisor = 1
-    for i in range(number):
-        factor = number % divisor
-        if factor == ("0"):
-            print(divisor)
-        divisor =+ 1
-operation()
+number = int(input("Please input the amount of numbers you're using: "))
+divisor = 1
+factors = []
+def operation(divisor):
+    for i in range(facNum):
+        factor = facNum % divisor
+        if factor == 0:
+         print(divisor)
+        divisor += 1
+for i in range(number):
+    facNum = int(input("Please input a number from your list of numbers to be factored: "))
+    operation(1)
+    factors.extend(divisor)
+gcf = max(factors)
+print(gcf)
