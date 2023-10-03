e30b2f98ba4f17329cb57f5d65cda8253890f110
Laithanator <144480666+Laithanator@users.noreply.github.com>
2023-10-03 13:26:13 -0400

    Update GCF.py

--- GCF.py
+++ GCF.py
@@ -1,5 +1,5 @@
-import math
+import cmath
 num1 = int(input("Please input your first number to be factored: "))
 num2 = int(input("Please input your second number to be factored: "))
-gcf = math.gcd(num1,num2)
+gcf = cmath.gcf(num1,num2)
 print(gcf)
